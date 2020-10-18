0ccb55e67597ebc7033fc0b093ca5951fa9f9bae
adambartyzal <adambartyzal@gmail.com>
2020-10-18 22:04:29 +0200

    name correction

--- stop.sh
+++ stop.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 exec </dev/null >> /var/log/calendar-event.log 2>&1
-su adam -c 'cd /home/adam/coding/python/calendar;python work_stop.py'
+su adam -c 'cd /home/adam/coding/python/calendar;python work_end.py'
